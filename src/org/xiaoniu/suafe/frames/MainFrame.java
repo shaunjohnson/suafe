8b2522ab2672696d928dfdf6b29d60db8b37908b
shaun.johnson <shaun.johnson@3a56d0cd-a219-0410-8d1a-59f2f7b8131d>
2008-03-18 18:15:40 +0000

    Refactor users popup menu.

--- src/org/xiaoniu/suafe/frames/MainFrame.java
+++ src/org/xiaoniu/suafe/frames/MainFrame.java
@@ -166,18 +166,10 @@ public class MainFrame extends BaseFrame implements ActionListener, FileOpener,
 	private JList userList = null;
 
 	private JMenuItem addGroupPopupMenuItem = null;
-		
-	private JMenuItem addRemoveMembersPopupMenuItem = null; 
-	
-	private JMenuItem addUserPopupMenuItem = null;  
-	private JMenuItem changeMembershipPopupMenuItem = null;
-	
+	private JMenuItem addRemoveMembersPopupMenuItem = null;	
 	private JMenuItem cloneGroupPopupMenuItem = null;
-	private JMenuItem cloneUserPopupMenuItem = null;
 	private JMenuItem deleteGroupPopupMenuItem = null;
-	private JMenuItem deleteUserPopupMenuItem = null;
 	private JMenuItem editGroupPopupMenuItem = null;
-	private JMenuItem editUserPopupMenuItem = null;
 	private JMenuItem printMenuItem = null;
 	
 	private JPanel accessRuleActionsPanel = null;
@@ -201,8 +193,7 @@ public class MainFrame extends BaseFrame implements ActionListener, FileOpener,
 	private JPanel userListPanel = null;	
 	
 	private JPopupMenu groupsPopupMenu = null;
-	private JPopupMenu usersPopupMenu = null; 
-
+	
 	private JScrollPane accessRulesScrollPane = null;
 	private JScrollPane accessRulesTreeScrollPane = null;
 	private JScrollPane groupAccessRulesScrollPane = null; 
@@ -230,6 +221,8 @@ public class MainFrame extends BaseFrame implements ActionListener, FileOpener,
 	private JTree accessRulesTree = null;
 	
 	private MainFrameMenuBar menuBar = null;
+	
+	private MainFrameUsersPopupMenu usersPopupMenu = null; 
 	
 	private Object[] groupAccessRulesColumnNames;
 	private Object[] pathAccessRulesColumnNames;	
@@ -930,10 +923,10 @@ public class MainFrame extends BaseFrame implements ActionListener, FileOpener,
 		getDeleteUserButton().setEnabled(enabled);
 		getChangeMembershipButton().setEnabled(changeMembershipEnabled);
 		
-		getEditUserPopupMenuItem().setEnabled(enabled);
-		getDeleteUserPopupMenuItem().setEnabled(enabled);
-		getChangeMembershipPopupMenuItem().setEnabled(changeMembershipEnabled);
-		getCloneUserPopupMenuItem().setEnabled(enabled);
+		usersPopupMenu.getEditUserPopupMenuItem().setEnabled(enabled);
+		usersPopupMenu.getDeleteUserPopupMenuItem().setEnabled(enabled);
+		usersPopupMenu.getChangeMembershipPopupMenuItem().setEnabled(changeMembershipEnabled);
+		usersPopupMenu.getCloneUserPopupMenuItem().setEnabled(enabled);
 	}
 	
 	/**
@@ -2357,12 +2350,7 @@ public class MainFrame extends BaseFrame implements ActionListener, FileOpener,
 	 */
 	private JPopupMenu getUsersPopupMenu() {
 		if (usersPopupMenu == null) {
-			usersPopupMenu = new JPopupMenu();
-			usersPopupMenu.add(getAddUserPopupMenuItem());
-			usersPopupMenu.add(getEditUserPopupMenuItem());
-			usersPopupMenu.add(getDeleteUserPopupMenuItem());
-			usersPopupMenu.add(getChangeMembershipPopupMenuItem());
-			usersPopupMenu.add(getCloneUserPopupMenuItem());
+			usersPopupMenu = new MainFrameUsersPopupMenu(this);
 			
 			// Add listener to the user list
 			MouseListener popupListener = new PopupListener(usersPopupMenu);
@@ -2370,81 +2358,6 @@ public class MainFrame extends BaseFrame implements ActionListener, FileOpener,
 		}
 		
 		return usersPopupMenu;
-	}
-
-	/**
-	 * This method initializes addUserPopupMenuItem.
-	 * 
-	 * @return javax.swing.JMenuItem
-	 */
-	private JMenuItem getAddUserPopupMenuItem() {
-		if (addUserPopupMenuItem == null) {
-			addUserPopupMenuItem = new JMenuItem();
-			addUserPopupMenuItem.addActionListener(this);
-			addUserPopupMenuItem.setActionCommand(Constants.ADD_USER_ACTION);
-			addUserPopupMenuItem.setIcon(ResourceUtil.addUserIcon);
-			addUserPopupMenuItem.setText(ResourceUtil.getString("button.add"));
-			addUserPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.adduser.tooltip"));
-		}
-		
-		return addUserPopupMenuItem;
-	}
-
-	/**
-	 * This method initializes editUserPopupMenuItem.
-	 * 
-	 * @return javax.swing.JMenuItem
-	 */
-	private JMenuItem getEditUserPopupMenuItem() {
-		if (editUserPopupMenuItem == null) {
-			editUserPopupMenuItem = new JMenuItem();
-			editUserPopupMenuItem.addActionListener(this);
-			editUserPopupMenuItem.setActionCommand(Constants.EDIT_USER_ACTION);
-			editUserPopupMenuItem.setIcon(ResourceUtil.editUserIcon);
-			editUserPopupMenuItem.setText(ResourceUtil.getString("button.edit"));
-			editUserPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.edituser.tooltip"));
-			editUserPopupMenuItem.setEnabled(false);
-		}
-		
-		return editUserPopupMenuItem;
-	}
-
-	/**
-	 * This method initializes deleteUserPopupMenuItem.
-	 * 
-	 * @return javax.swing.JMenuItem
-	 */
-	private JMenuItem getDeleteUserPopupMenuItem() {
-		if (deleteUserPopupMenuItem == null) {
-			deleteUserPopupMenuItem = new JMenuItem();
-			deleteUserPopupMenuItem.addActionListener(this);
-			deleteUserPopupMenuItem.setActionCommand(Constants.DELETE_USER_ACTION);
-			deleteUserPopupMenuItem.setIcon(ResourceUtil.deleteUserIcon);
-			deleteUserPopupMenuItem.setText(ResourceUtil.getString("button.delete"));
-			deleteUserPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.deleteuser.tooltip"));
-			deleteUserPopupMenuItem.setEnabled(false);
-		}
-		
-		return deleteUserPopupMenuItem;
-	}
-
-	/**
-	 * This method initializes changeMembershipPopupMenuItem.
-	 * 
-	 * @return javax.swing.JMenuItem
-	 */
-	private JMenuItem getChangeMembershipPopupMenuItem() {
-		if (changeMembershipPopupMenuItem == null) {
-			changeMembershipPopupMenuItem = new JMenuItem();
-			changeMembershipPopupMenuItem.addActionListener(this);
-			changeMembershipPopupMenuItem.setActionCommand(Constants.CHANGE_MEMBERSHIP_ACTION);
-			changeMembershipPopupMenuItem.setIcon(ResourceUtil.changeMembershipIcon);
-			changeMembershipPopupMenuItem.setText(ResourceUtil.getString("mainframe.button.changemembership"));
-			changeMembershipPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.changemembership.tooltip"));
-			changeMembershipPopupMenuItem.setEnabled(false);
-		}
-		
-		return changeMembershipPopupMenuItem;
 	}
 
 	/**
@@ -3649,22 +3562,6 @@ public class MainFrame extends BaseFrame implements ActionListener, FileOpener,
 		return groupListPanel;
 	}
 	
-	/**
-	 * This method initializes cloneUserPopupMenuItem	
-	 * 	
-	 * @return javax.swing.JMenuItem	
-	 */    
-	private JMenuItem getCloneUserPopupMenuItem() {
-		if (cloneUserPopupMenuItem == null) {
-			cloneUserPopupMenuItem = new JMenuItem();
-			cloneUserPopupMenuItem.addActionListener(this);
-			cloneUserPopupMenuItem.setActionCommand(Constants.CLONE_USER_ACTION);
-			cloneUserPopupMenuItem.setIcon(ResourceUtil.cloneUserIcon);
-			cloneUserPopupMenuItem.setText(ResourceUtil.getString("menu.clone"));
-			cloneUserPopupMenuItem.setEnabled(false);
-		}
-		return cloneUserPopupMenuItem;
-	}
 	/**
 	 * This method initializes cloneGroupPopupMenuItem	
 	 * 	
---
+++ src/org/xiaoniu/suafe/frames/MainFrameUsersPopupMenu.java
@@ -0,0 +1,130 @@
+package org.xiaoniu.suafe.frames;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+import org.xiaoniu.suafe.Constants;
+import org.xiaoniu.suafe.resources.ResourceUtil;
+
+public class MainFrameUsersPopupMenu extends JPopupMenu {
+
+	private static final long serialVersionUID = 6018077204492947280L;
+
+	private ActionListener actionListener;
+	
+	private JMenuItem addUserPopupMenuItem = null;  
+	
+	private JMenuItem cloneUserPopupMenuItem = null;
+	
+	private JMenuItem deleteUserPopupMenuItem = null;
+	
+	private JMenuItem editUserPopupMenuItem = null;
+	
+	private JMenuItem changeMembershipPopupMenuItem = null;
+	
+	public MainFrameUsersPopupMenu(ActionListener actionListener) {
+		super();
+		
+		this.actionListener = actionListener;
+		
+		add(getAddUserPopupMenuItem());
+		add(getEditUserPopupMenuItem());
+		add(getDeleteUserPopupMenuItem());
+		add(getChangeMembershipPopupMenuItem());
+		add(getCloneUserPopupMenuItem());
+	}
+	
+	/**
+	 * This method initializes addUserPopupMenuItem.
+	 * 
+	 * @return javax.swing.JMenuItem
+	 */
+	public JMenuItem getAddUserPopupMenuItem() {
+		if (addUserPopupMenuItem == null) {
+			addUserPopupMenuItem = new JMenuItem();
+			addUserPopupMenuItem.addActionListener(actionListener);
+			addUserPopupMenuItem.setActionCommand(Constants.ADD_USER_ACTION);
+			addUserPopupMenuItem.setIcon(ResourceUtil.addUserIcon);
+			addUserPopupMenuItem.setText(ResourceUtil.getString("button.add"));
+			addUserPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.adduser.tooltip"));
+		}
+		
+		return addUserPopupMenuItem;
+	}
+
+	/**
+	 * This method initializes editUserPopupMenuItem.
+	 * 
+	 * @return javax.swing.JMenuItem
+	 */
+	public JMenuItem getEditUserPopupMenuItem() {
+		if (editUserPopupMenuItem == null) {
+			editUserPopupMenuItem = new JMenuItem();
+			editUserPopupMenuItem.addActionListener(actionListener);
+			editUserPopupMenuItem.setActionCommand(Constants.EDIT_USER_ACTION);
+			editUserPopupMenuItem.setIcon(ResourceUtil.editUserIcon);
+			editUserPopupMenuItem.setText(ResourceUtil.getString("button.edit"));
+			editUserPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.edituser.tooltip"));
+			editUserPopupMenuItem.setEnabled(false);
+		}
+		
+		return editUserPopupMenuItem;
+	}
+
+	/**
+	 * This method initializes deleteUserPopupMenuItem.
+	 * 
+	 * @return javax.swing.JMenuItem
+	 */
+	public JMenuItem getDeleteUserPopupMenuItem() {
+		if (deleteUserPopupMenuItem == null) {
+			deleteUserPopupMenuItem = new JMenuItem();
+			deleteUserPopupMenuItem.addActionListener(actionListener);
+			deleteUserPopupMenuItem.setActionCommand(Constants.DELETE_USER_ACTION);
+			deleteUserPopupMenuItem.setIcon(ResourceUtil.deleteUserIcon);
+			deleteUserPopupMenuItem.setText(ResourceUtil.getString("button.delete"));
+			deleteUserPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.deleteuser.tooltip"));
+			deleteUserPopupMenuItem.setEnabled(false);
+		}
+		
+		return deleteUserPopupMenuItem;
+	}
+
+	/**
+	 * This method initializes changeMembershipPopupMenuItem.
+	 * 
+	 * @return javax.swing.JMenuItem
+	 */
+	public JMenuItem getChangeMembershipPopupMenuItem() {
+		if (changeMembershipPopupMenuItem == null) {
+			changeMembershipPopupMenuItem = new JMenuItem();
+			changeMembershipPopupMenuItem.addActionListener(actionListener);
+			changeMembershipPopupMenuItem.setActionCommand(Constants.CHANGE_MEMBERSHIP_ACTION);
+			changeMembershipPopupMenuItem.setIcon(ResourceUtil.changeMembershipIcon);
+			changeMembershipPopupMenuItem.setText(ResourceUtil.getString("mainframe.button.changemembership"));
+			changeMembershipPopupMenuItem.setToolTipText(ResourceUtil.getString("mainframe.button.changemembership.tooltip"));
+			changeMembershipPopupMenuItem.setEnabled(false);
+		}
+		
+		return changeMembershipPopupMenuItem;
+	}
+	
+	/**
+	 * This method initializes cloneUserPopupMenuItem	
+	 * 	
+	 * @return javax.swing.JMenuItem	
+	 */    
+	public JMenuItem getCloneUserPopupMenuItem() {
+		if (cloneUserPopupMenuItem == null) {
+			cloneUserPopupMenuItem = new JMenuItem();
+			cloneUserPopupMenuItem.addActionListener(actionListener);
+			cloneUserPopupMenuItem.setActionCommand(Constants.CLONE_USER_ACTION);
+			cloneUserPopupMenuItem.setIcon(ResourceUtil.cloneUserIcon);
+			cloneUserPopupMenuItem.setText(ResourceUtil.getString("menu.clone"));
+			cloneUserPopupMenuItem.setEnabled(false);
+		}
+		return cloneUserPopupMenuItem;
+	}
+}
