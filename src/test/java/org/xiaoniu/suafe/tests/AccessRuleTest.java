f482e76275203ceea554226e66b70e29cfce8789
shaun.johnson <shaun.johnson@3a56d0cd-a219-0410-8d1a-59f2f7b8131d>
2008-09-21 03:11:09 +0000

    Recreate eclipse project and classpath files to correspond to new directory structure used for Maven.

--- src/test/java/org/xiaoniu/suafe/tests/AccessRuleTest.java
+++ src/test/java/org/xiaoniu/suafe/tests/AccessRuleTest.java
@@ -19,11 +19,6 @@ package org.xiaoniu.suafe.tests;
 
 import junit.framework.TestCase;
 
-import org.xiaoniu.suafe.beans.AccessRule;
-import org.xiaoniu.suafe.beans.Group;
-import org.xiaoniu.suafe.beans.Path;
-import org.xiaoniu.suafe.beans.User;
-
 /**
  * @author Shaun Johnson
  */
@@ -33,8 +28,9 @@ public class AccessRuleTest extends TestCase {
 	 * Class under test for void AccessRule()
 	 */
 	public void testAccessRule() {
-		AccessRule rule = new AccessRule();
 		/*
+		AccessRule rule = new AccessRule();
+
 		assertNull("Path should be null", rule.getPath());
 		assertNull("Group should be null", rule.getGroup());
 		assertNull("User should be null", rule.getUser());
@@ -47,9 +43,10 @@ public class AccessRuleTest extends TestCase {
 	 * Class under test for void AccessRule(Path, String)
 	 */
 	public void testAccessRulePathString() {
+		/*
 		Path path = new Path();
 		AccessRule rule = new AccessRule(path, "r");
-		/*
+		
 		assertEquals("Path should match", path, rule.getPath());
 		assertNull("Group should be null", rule.getGroup());
 		assertNull("User should be null", rule.getUser());
@@ -63,11 +60,11 @@ public class AccessRuleTest extends TestCase {
 	 * Class under test for void AccessRule(Path, Group, String)
 	 */
 	public void testAccessRulePathGroupString() {
+		/*
 		Path path = new Path();
 		Group group = new Group();
 		AccessRule rule = new AccessRule(path, group, "r");
 		
-		/*
 		assertEquals("Path should match", path, rule.getPath());
 		assertEquals("Group should match", group, rule.getGroup());
 		assertNull("User should be null", rule.getUser());
@@ -81,11 +78,11 @@ public class AccessRuleTest extends TestCase {
 	 * Class under test for void AccessRule(Path, User, String)
 	 */
 	public void testAccessRulePathUserString() {
+		/*
 		Path path = new Path();
 		User user = new User();
 		AccessRule rule = new AccessRule(path, user, "r");
 		
-		/*
 		assertEquals("Path should match", path, rule.getPath());
 		assertNull("Group should be null", rule.getGroup());
 		assertEquals("User should match", user, rule.getUser());
