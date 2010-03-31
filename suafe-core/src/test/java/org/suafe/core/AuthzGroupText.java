904ebb65a0254536cc18b415a37ad9b198dd4d17
shaun.johnson <shaun.johnson@3a56d0cd-a219-0410-8d1a-59f2f7b8131d>
2010-03-31 23:59:57 +0000

    Implement basic unit tests for AuthzGroup class.

--- suafe-core/src/test/java/org/suafe/core/AuthzGroupText.java
+++ suafe-core/src/test/java/org/suafe/core/AuthzGroupText.java
@@ -1,6 +1,7 @@
 package org.suafe.core;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -10,16 +11,27 @@ import org.junit.Test;
 public class AuthzGroupText {
 	@Test
 	public void testAuthzGroupString() {
-		fail();
+		final AuthzGroup authzGroup = new AuthzGroup("name");
+
+		assertEquals("name should be valid", "name", authzGroup.getName());
 	}
 
 	@Test
 	public void testCompareTo() {
-		fail();
+		assertTrue("Null groups should match", new AuthzGroup(null).compareTo(new AuthzGroup(null)) == 0);
+		assertTrue("Empty groups should match", new AuthzGroup("").compareTo(new AuthzGroup("")) == 0);
+		assertTrue("Groups with same name should match", new AuthzGroup("name").compareTo(new AuthzGroup("name")) == 0);
+		assertTrue("Groups with same name and alias should match", new AuthzGroup("name").compareTo(new AuthzGroup(
+				"name")) == 0);
+
+		assertTrue("Groups should not match", new AuthzGroup("name").compareTo(new AuthzGroup("same")) < 0);
+		assertTrue("Groups should not match", new AuthzGroup("same").compareTo(new AuthzGroup("name")) > 0);
 	}
 
 	@Test
 	public void testToString() {
-		fail();
+		final AuthzGroup authzGroup = new AuthzGroup("myName");
+
+		assertTrue("toString() should output name", authzGroup.getName().contains("myName"));
 	}
 }
