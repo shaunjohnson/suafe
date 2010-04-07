ee8a7817ca849e6a6aee72d9a38197346296c5bd
shaun.johnson <shaun.johnson@3a56d0cd-a219-0410-8d1a-59f2f7b8131d>
2010-04-07 01:02:52 +0000

    Add tests for resource key equals() and hashCode() methods.

--- suafe-core/src/test/java/org/suafe/core/utilities/AuthzErrorResourceKeyTest.java
+++ suafe-core/src/test/java/org/suafe/core/utilities/AuthzErrorResourceKeyTest.java
@@ -1,6 +1,8 @@
 package org.suafe.core.utilities;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.lang.reflect.Field;
@@ -28,5 +30,30 @@ public class AuthzErrorResourceKeyTest {
 		catch (final IllegalAccessException e) {
 			fail("IllegalAccessException caught");
 		}
+	}
+
+	@Test
+	public void testEquals() {
+		assertTrue("Values should match", AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP
+				.equals(AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP));
+		assertFalse("Values should not match", AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP
+				.equals(AuthzErrorResourceKey.GROUP_ALREADY_EXISTS));
+
+		// Test invalid values
+		assertFalse("Values should not match", AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP.equals(null));
+		assertFalse("Values should not match", AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP.equals(""));
+		assertFalse("Values should not match", AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP
+				.equals(AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP.toString()));
+	}
+
+	@Test
+	public void testHashCode() {
+		assertTrue(
+				"HashCode values should match",
+				AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP.hashCode() == AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP
+						.hashCode());
+		assertFalse("HashCode values should not match",
+				AuthzErrorResourceKey.ALREADY_MEMBER_OF_GROUP.hashCode() == AuthzErrorResourceKey.GROUP_ALREADY_EXISTS
+						.hashCode());
 	}
 }
