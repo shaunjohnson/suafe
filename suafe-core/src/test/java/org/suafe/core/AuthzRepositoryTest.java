aea344c2dbb2358a069ee4025bcca1da6dedce09
shaun.johnson <shaun.johnson@3a56d0cd-a219-0410-8d1a-59f2f7b8131d>
2010-04-09 02:05:42 +0000

    Add tests for AuthzRepository equals() and hashCode() methods.

--- suafe-core/src/test/java/org/suafe/core/AuthzRepositoryTest.java
+++ suafe-core/src/test/java/org/suafe/core/AuthzRepositoryTest.java
@@ -1,6 +1,7 @@
 package org.suafe.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -30,6 +31,28 @@ public class AuthzRepositoryTest {
 				new AuthzRepository("name").compareTo(new AuthzRepository("same")) < 0);
 		assertTrue("Repositories should not match",
 				new AuthzRepository("same").compareTo(new AuthzRepository("name")) > 0);
+	}
+
+	@Test
+	public void testEquals() {
+		assertTrue("Values should match", new AuthzRepository("name").equals(new AuthzRepository("name")));
+		assertTrue("Values should match", new AuthzRepository(null).equals(new AuthzRepository(null)));
+		assertFalse("Values should not match", new AuthzRepository("name").equals(new AuthzRepository("name2")));
+		assertFalse("Values should not match", new AuthzRepository(null).equals(new AuthzRepository("name2")));
+
+		// Test invalid values
+		assertFalse("Values should not match", new AuthzRepository("name").equals(null));
+		assertFalse("Values should not match", new AuthzRepository("name").equals(""));
+		assertFalse("Values should not match", new AuthzRepository("name").equals(new AuthzRepository("name")
+				.toString()));
+	}
+
+	@Test
+	public void testHashCode() {
+		assertTrue("HashCode values should match",
+				new AuthzRepository("name").hashCode() == new AuthzRepository("name").hashCode());
+		assertFalse("HashCode values should not match", new AuthzRepository("name").hashCode() == new AuthzRepository(
+				"name2").hashCode());
 	}
 
 	@Test
