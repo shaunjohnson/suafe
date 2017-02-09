41dd29dc350120966e7548f12c3b4cebe67572b7
Shaun Johnson <shaun@lmxm.net>
2017-02-09 11:00:59 -0500

    Updated license from GPLv2 to GPLv3 and updated copyright notices

--- src/main/java/org/xiaoniu/suafe/dialogs/DialogUtil.java
+++ src/main/java/org/xiaoniu/suafe/dialogs/DialogUtil.java
@@ -1,19 +1,20 @@
-/**
- * @copyright
- * ====================================================================
- * Copyright (c) 2006 Xiaoniu.org.  All rights reserved.
+/*
+ * Copyright (c) 2006-2017 by LMXM LLC <suafe@lmxm.net>
  *
- * This software is licensed as described in the file LICENSE, which
- * you should have received as part of this distribution.  The terms
- * are also available at http://code.google.com/p/suafe/.
- * If newer versions of this license are posted there, you may use a
- * newer version instead, at your option.
+ * This file is part of Suafe.
  *
- * This software consists of voluntary contributions made by many
- * individuals.  For exact contribution history, see the revision
- * history and logs, available at http://code.google.com/p/suafe/.
- * ====================================================================
- * @endcopyright
+ * Suafe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Suafe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Suafe.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.xiaoniu.suafe.dialogs;
 
